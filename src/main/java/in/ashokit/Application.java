bb2994aed113bf1ddd58be21af278f6f37b3c073
Abu <abu9140sufiyan@gmail.com>
2024-02-10 13:03:11 +0530

    added new method m2()

--- src/main/java/in/ashokit/Application.java
+++ src/main/java/in/ashokit/Application.java
@@ -11,6 +11,10 @@ public class Application {
 		System.out.println("m1  ");
 	}
 
+	public static void m2(){
+		System.out.println("m2()  ");
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
