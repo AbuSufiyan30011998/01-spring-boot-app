63a07c8308a4a4250c455516eecb6c14a1bdaa13
Abu <abu9140sufiyan@gmail.com>
2024-02-10 12:32:23 +0530

    add static variable

--- src/main/java/in/ashokit/Application.java
+++ src/main/java/in/ashokit/Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 
-	public void m1(){
+	public static void m1(){
 		System.out.println("m1  ");
 	}
 
